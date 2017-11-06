5be0d49399aeae0e8df8a71397e1144372793202
Milos Lazic <mlazic95@hotmail.com>
2017-11-05 22:27:56 -0800

    mlazic: first lookup table generated

--- lut/MATLAB/lut.m
+++ lut/MATLAB/lut.m
@@ -1,19 +1,55 @@
 clear; clc;
 
 
-% center of workspace
-px = 50; % dummy tooltip coordinate x (for testing)
-py = 319.95;  % dummy tooltip coordinate y (for testing)
+% % center of workspace
+% px = 50; % dummy tooltip coordinate x (for testing)
+% py = 319.95;  % dummy tooltip coordinate y (for testing)
 
 
-% Rotation matrix; axes B (workspace axes) with respect to axes A
+% Rotation matrix; axes W (workspace axes) with respect to axes A
 % (manipulator base axes)
-R_AB = transpose([1 0; 0 -1]);
+R_AW = transpose([1 0; 0 -1]);
 
-% Position vector; origin axis A to origin axis B; with respect to axis A
-P_AB = transpose([-229.40 427.90]);
+% Position vector; origin axis A to origin axis W; with respect to axis A
+P_AW = transpose([-89.7 427.90]);
 
+P_AP = P_AW + R_AW*[139;107];
 
-[th1, th3] = invkinfxn([-89.7, 427.90])
+[th1 th2] = invkinfxn( P_AP);
 
+fileID = fopen('table.txt', 'w');
 
+fprintf(fileID, 'struct pos table[][] =\r\n');
+fprintf(fileID, '{\r\n');
+
+X_LIMIT = 279;
+Y_LIMIT = 215;
+
+for x=0:1:X_LIMIT
+    
+    fprintf( fileID, '     { ');
+    
+    for y=0:1:Y_LIMIT
+        
+        P_AP = P_AW + R_AW*[x;y];
+        
+        [th1, th3] = invkinfxn( P_AP);
+        
+        
+        fprintf(fileID, '{%.0f, %.0f}', 1000*th1, 1000*th3);
+        
+        if y < Y_LIMIT
+            fprintf(fileID, ', ');
+        else
+            fprintf(fileID, ' ');
+        end
+        
+    end
+    
+    fprintf(fileID, '}, \r\n');
+    
+end
+
+fprintf(fileID, '};\r\n');
+
+fclose(fileID);

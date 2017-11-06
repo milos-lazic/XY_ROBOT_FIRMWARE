1ed6b06080ade48fb870eefb69e6818a5ce3f2b7
Milos <mlazic95@htomail.com>
2017-11-05 23:20:56 -0800

    mlazic: lookup table program

--- testing/autocommand.py
+++ testing/autocommand.py
@@ -8,9 +8,9 @@ BUFFER_SIZE = 8
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
 
-for x in xrange(0, 100):
-	time.sleep(0.1)
-	s.send('AT!MTR_CMD=GOTO,1,1')
+for x in xrange(0, 1000):
+	time.sleep(0.0005)
+	s.send('AT!MTR_CMD=GOTO,5,5')
 	data = s.recv(BUFFER_SIZE)
 	print "received:", data
 

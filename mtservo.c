10a8ff0d8c4e288763d10c85fbe2599b2a510f5c
Milos <mlazic95@htomail.com>
2017-11-09 14:12:56 -0800

    mlazic

--- mtservo.c
+++ mtservo.c
@@ -32,3 +32,30 @@ void mtservo_setPwmFreq( int fd, unsigned int pwm_freq)
         wiringPiI2CWriteReg8( fd, MODE1_REG_ADR, oldmode | 0x80);
 }
 
+
+void mtservo_setDuty( int fd, int channel, int on, int off)
+{
+	wiringPiI2CWriteReg8( fd, LED0_ON_L_REG_ADR+4*channel, on & 0xFF);
+	wiringPiI2CWriteReg8( fd, LED0_ON_H_REG_ADR+4*channel, on >> 8);
+	wiringPiI2CWriteReg8( fd, LED0_OFF_L_REG_ADR+4*channel, off & 0xFF);
+	wiringPiI2CWriteReg8( fd, LED0_OFF_H_REG_ADR+4*channel, off >> 8);
+}
+
+
+void mtservo_init( int fd)
+{
+	int mode1;
+
+	// zero all PWM ports
+	mtservo_resetAllPwm( fd, 0, 0);
+
+	wiringPiI2CWriteReg8( fd, MODE2_REG_ADR, __OUTDRV);
+	wiringPiI2CWriteReg8( fd, MODE1_REG_ADR, __ALLCALL);
+
+	mode1 = wiringPiI2CReadReg8( fd, MODE1_REG_ADR);
+	mode1 = mode1 & (~__SLEEP);
+	wiringPiI2CWriteReg8( fd, MODE1_REG_ADR, mode1);
+
+	mtservo_setPwmFreq( fd, 50);
+}
+

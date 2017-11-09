8a4697aab1437c05453a859b73df6b01359cec2a
Milos <mlazic95@htomail.com>
2017-11-09 13:58:22 -0800

    mlazic

--- mtservo.c
+++ mtservo.c
@@ -11,3 +11,24 @@ void mtservo_resetAllPWM( int fd, int on, int off)
 	wiringPiI2CWriteReg8( fd, ALL_LED_OFF_H_REG_ADR, off >> 8);
 }
 
+
+void mtservo_setPwmFreq( int fd, unsigned int pwm_freq);
+{
+	unsigned int prescaleval = 25000000;
+        int oldmode, newmode;
+
+        prescaleval /= 4096;
+        prescaleval /= freq_hz;
+        prescaleval -= 1;
+
+        oldmode = wiringPiI2CReadReg8( fd, MODE1_REG_ADR);
+        newmode = (oldmode & 0x7F) | 0x10; // sleep
+
+        wiringPiI2CWriteReg8( fd, MODE1_REG_ADR, newmode);
+        wiringPiI2CWriteReg8( fd, PRE_SCALE_REG_ADR, prescaleval);
+        wiringPiI2CWriteReg8( fd, MODE1_REG_ADR, oldmode);
+
+
+        wiringPiI2CWriteReg8( fd, MODE1_REG_ADR, oldmode | 0x80);
+}
+

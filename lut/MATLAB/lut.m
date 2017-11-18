c68e5c8af52faf4dbc6477990d39cb5cf837bd46
Milos Lazic <mlazic95@hotmail.com>
2017-11-17 18:51:14 -0800

    mlazic

--- lut/MATLAB/lut.m
+++ lut/MATLAB/lut.m
@@ -12,7 +12,7 @@ R_AW = transpose([1 0; 0 -1]);
 
 % Position vector; origin axis A to origin axis W; with respect to axis A
 % P_AW = transpose([-89.7 408]);
-P_AW = transpose([-43.18 350]);
+P_AW = transpose([-43.18 355]);
 
 % Position vector; origin axis A to point P; with respect to axis A
 P_AP = P_AW + R_AW*[139;107];
